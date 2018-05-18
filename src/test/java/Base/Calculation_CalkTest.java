83d34356baf3c0df3759eb78ce23108d341c5095
16130174 <Bubblegum.no@yandex.ru>
2018-05-18 20:28:12 +0500

    Добавление тестов студенткой Токаревой

---
+++ src/test/java/Base/Calculation_CalkTest.java
@@ -0,0 +1,37 @@
+package Base;				/** Указываем на то, что класс принадлежит пакету */
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Тест формулы расчета срока вложения
+ * @author Токарева, Портнов
+ *
+ */
+class Calculation_CalkTest {
+
+    @Test
+    void sroc() {
+        assertEquals(30.0, Calculation.Calk(15000, 12000, 10));
+    }
+    
+    @Test
+    void sroc1() {
+        assertEquals(1380.0, Calculation.Calk(15000, 1200, 10));
+    }
+    
+    @Test
+    void sroc2() {
+        assertEquals(1380.0, Calculation.Calk(15000, 1200, 10));
+    }
+  
+    @Test
+    void sroc3() {
+        assertEquals(30.0, Calculation.Calk(15000, 12000, 10));
+    }
+    
+    @Test
+    void sroc4() {
+        assertEquals(155.3986231401288, Calculation.Calk(265000, 90060, 15));
+    }
+}
