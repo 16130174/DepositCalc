5950541b012bc8148a2f4c5246d77f61a3250753
vernipoltos14 <vladislav_p101@mail.ru>
2018-05-19 12:48:33 +0500

    Добавление тестов студентом Портновым

--- src/test/java/Base/Calculation_CalkTest.java
+++ src/test/java/Base/Calculation_CalkTest.java
@@ -34,4 +34,30 @@ class Calculation_CalkTest {
     void sroc4() {
         assertEquals(155.3986231401288, Calculation.Calk(265000, 90060, 15));
     }
+    @Test
+    void sroc5() {
+        assertEquals(70426.66666666667, Calculation.Calk(26500, 90, 5));
+    }
+
+    @Test
+    void sroc6() {
+        assertEquals(438.848254791223, Calculation.Calk(54930, 15430, 7));
+    }
+    
+    @Test
+    void sroc7() {
+        assertEquals(428.94784995425437, Calculation.Calk(150000, 32790, 10));
+    }
+    
+    @Test
+    void sroc8() {
+        assertEquals(32670.0, Calculation.Calk(327900, 1200, 10));
+    }
+    
+    @Test
+    void sroc9() {
+        assertEquals(48885.0, Calculation.Calk(6550, 32, 5));
+    }
+    
+
 }
